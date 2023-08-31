2b6300a1cf1da480b4f45a46cbabafa58412d094
vishwas1 <vishwasanand.cse@gmail.com>
2023-08-31 11:48:02 +0530

    updated client fiel for singleton usecase

--- src/singleton/client.ts
+++ src/singleton/client.ts
@@ -1,21 +1,20 @@
 import Game1 from './game1';
 import Game2 from './game2';
 import Game3 from './game3';
-import LeaderBoard from './leaderboard';
 
-const leaderboard = new LeaderBoard();
 const game1 = new Game1();
-game1.addWinner( 1, 'vishwas')
+game1.addWinner(2, 'vishwas')
 
 const game2 = new Game2();
-game2.addWinner(3, 'vishwas')
+game2.addWinner(1, 'amit')
 
 const game3 = new Game3();
-game3.addWinner(2, 'vishwas')
+game3.addWinner(3, 'rohit')
 
-
-
-
+/// See all games are refering to the smae leaderboard because of singleton
+game1.leaderBoard.print()
+game2.leaderBoard.print()
+game3.leaderBoard.print()
 
 
 
